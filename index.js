2530b8afada897312113a41a233c2c2ecd1179ea
ghigbie <gehigbie@gmail.com>
2018-04-20 20:46:49 -0600

    Save changes

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
     console.log('Electron is running');
     const mainWindow = new BrowserWindow ({}); //the mpty object is for configuration options
     mainWindow.loadURL(`file://(${__dirname}/index.html`);
 
-    ipcMain.on('video:submit', () => { //ipcMain.on receives the information sent from ipcRenderer in the window
-
+    ipcMain.on('video:submit', (event, path) => { //ipcMain.on receives the information sent from ipcRenderer in the window
+        ffmpeg.ffprobe(path, (err, metadata) => {
+            console.log('Video duration is:', metadata.format.duration);
+        });
     });
 });
