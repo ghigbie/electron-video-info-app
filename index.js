7f221c1156ddfbbf0e6858e4828bdcdaab0b54d5
ghigbie <gehigbie@gmail.com>
2018-04-20 20:31:00 -0600

    Log video data

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 const { app, BrowserWindow } = electron;
 
 app.on('ready', () => {
     console.log('Electron is running');
     const mainWindow = new BrowserWindow ({}); //the mpty object is for configuration options
     mainWindow.loadURL(`file://(${__dirname}/index.html`);
+
+    ipcMain.on('video:submit', () => { //ipcMain.on receives the information sent from ipcRenderer in the window
+
+    });
 });
